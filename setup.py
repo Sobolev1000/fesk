5464d41130af307f03730fd90b217665473c9db9
Sobolev1000 <100231086+Sobolev1000@users.noreply.github.com>
2023-07-08 22:50:27 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fesk',
+      version='0.1',
+      description='New pip by qSobolev',
+      packages=['asyncio'],
+      author_email='minecraftsobolev@gmail.com',
+      zip_safe=False)
