c03991b524d80cb095f502bc9c575bea7d1852f6
Sobolev1000 <100231086+Sobolev1000@users.noreply.github.com>
2023-07-08 22:59:11 +0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from .fesk import fesk
+import requests
+import json
+
+
+#information
+__title__ = "Fesk"
+
+__author__ = "qSobolev"
+
+__license__ = "MIT"
+
+__copyright__ = "Copyright 2023 qSobolev"
+
+__version__ = '0.0.1'
+
+__status__ = "(Beta)"
+
+print(f"""
+{__title__} {__version__}{__status__} made by {__author__}\n
+""")
